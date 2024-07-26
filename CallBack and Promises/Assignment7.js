8bd95d0bb610bd76acd87e7ccc3b21f62765e640
Pratul Makar <kk953504@gmail.com>
2024-07-26 20:32:38 +0530

    new

---
+++ CallBack and Promises/Assignment7.js
@@ -0,0 +1,9 @@
+/**
+Assignment 7
+Write a function fetchMultipleData that takes an array of URLs and uses Promise.all() to fetch data from all the
+URLs concurrently. Return an array of responses.
+ */
+
+async function fetchMultipleData(arrayOfURLs) {
+    const url =
+}
