074ed6a6fb135ebe4dac20108390efad97078b1d
Pratul Makar <kk953504@gmail.com>
2024-07-27 00:22:21 +0530

    new

--- CallBack and Promises/Assignment7.js
+++ CallBack and Promises/Assignment7.js
@@ -4,6 +4,23 @@ Write a function fetchMultipleData that takes an array of URLs and uses Promise.
 URLs concurrently. Return an array of responses.
  */
 
-async function fetchMultipleData(arrayOfURLs) {
-    const url =
+async function fetchMultipleData(urls) {
+    const promises = urls.map(url => fetch(url).then(res => res.json()));
+    return Promise.all(promises);
 }
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/todos/1',
+    'https://jsonplaceholder.typicode.com/todos/2',
+    'https://jsonplaceholder.typicode.com/todos/3',
+    'https://jsonplaceholder.typicode.com/todos/4',
+    'https://jsonplaceholder.typicode.com/todos/5',
+];
+
+fetchMultipleData(urls)
+    .then(responses => {
+        console.log(responses);
+    })
+    .catch(error => {
+        console.error(error);
+    });
